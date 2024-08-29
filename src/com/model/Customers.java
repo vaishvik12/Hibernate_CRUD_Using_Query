39a88fe73469e2de0b84b26255de34cb0dcdc8bb
vaishvik12 <www.vaishvikjamalta@gmail.com>
2024-08-29 19:52:12 +0530

    added cache concurrency strategy. read_only

--- src/com/model/Customers.java
+++ src/com/model/Customers.java
@@ -6,7 +6,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+
+
 @Entity
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
 public class Customers {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
